4477091dae278840f0ddc6a2ee06d634e2e76e83
alkatedm-dev <alkatedm@gmail.com>
2026-04-13 22:46:47 +0300

    Update app.py
    import telebot
    from flask import Flask, request
    import os
    
    TOKEN = '8537913433:AAHebXBap3cDIzvJEE6ZClIDL9PZ88Lzqz0'
    bot = telebot.TeleBot(TOKEN)
    app = Flask(__name__)
    
    @app.route('/' + TOKEN, methods=['POST'])
    def getMessage():
        json_string = request.get_data().decode('utf-8')
        update = telebot.types.Update.de_json(json_string)
        bot.process_new_updates([update])
        return "!", 200
    
    @app.route('/')
    def webhook():
        bot.remove_webhook()
        bot.set_webhook(url='https://kosay-and-ahmad-1.onrender.com/' + TOKEN)
        return "OK", 200
    
    @bot.message_handler(func=lambda message: True)
    def echo_all(message):
        bot.reply_to(message, "أخيراً يا قصي! أنا سمعتك!")
    
    if __name__ == "__main__":
        app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
 import telebot
-import os
 from flask import Flask, request
+import os
 
 TOKEN = '8537913433:AAHebXBap3cDIzvJEE6ZClIDL9PZ88Lzqz0'
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+app = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "أهلاً بك يا قصي! أنا شغال هلق.")
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, f"وصلت رسالتك: {message.text}")
-
-@server.route('/' + TOKEN, methods=['POST'])
+@app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
 
-@server.route("/")
+@app.route('/')
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://kosay-and-ahmad-1.onrender.com/' + TOKEN)
-    return "البوت يعمل بنجاح!", 200
+    return "OK", 200
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "أخيراً يا قصي! أنا سمعتك!")
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
     
