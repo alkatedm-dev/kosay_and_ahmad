edec86769fd5e2fdb35b5df6af14f2724147849f
alkatedm-dev <alkatedm@gmail.com>
2026-04-14 10:39:16 +0300

    Update app.py
    import telebot
    import time
    
    TOKEN = '8537913433:AAHebXBap3cDIzvJEE6ZClIDL9PZ88Lzqz0'
    bot = telebot.TeleBot(TOKEN)
    
    @bot.message_handler(commands=['start'])
    def start(message):
        bot.reply_to(message, "أهلاً يا قصي! هاد الكود الجديد شغال غصب عن الكل. كيف حالك؟")
    
    @bot.message_handler(func=lambda message: True)
    def echo_all(message):
        bot.reply_to(message, f"وصلتني رسالتك: {message.text}")
    
    print("البوت بدأ العمل بنظام الـ Polling...")
    bot.infinity_polling()

--- app.py
+++ app.py
@@ -1,32 +1,16 @@
 import telebot
-from flask import Flask, request
-import os
+import time
 
 TOKEN = '8537913433:AAHebXBap3cDIzvJEE6ZClIDL9PZ88Lzqz0'
 bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "صباح الخير يا قصي! أخيراً اشتغلت.")
+    bot.reply_to(message, "أهلاً يا قصي! هاد الكود الجديد شغال غصب عن الكل. كيف حالك؟")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, "وصلت رسالتك يا بطل!")
+    bot.reply_to(message, f"وصلتني رسالتك: {message.text}")
 
-@app.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-@app.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://kosay-and-ahmad-1.onrender.com/' + TOKEN)
-    return "البوت يعمل بنجاح!", 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-    
+print("البوت بدأ العمل بنظام الـ Polling...")
+bot.infinity_polling()
